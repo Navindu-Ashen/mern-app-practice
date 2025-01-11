1a0e19267d66eb8ca22e6a4e0508ee151c7792c2
Navindu-Ashen <navindu158@gmail.com>
2025-01-11 16:04:02 +0530

    fix(error): fixes the error that make for check workflows

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -4,7 +4,7 @@ import ItemCard from "../components/itemCard";
 import { useEffect, useState } from "react";
 import { useProductStore } from "../stores/product";
 import { toaster } from "../components/ui/toaster";
-import EditProductDialog from "../components/test/EditProductDialog";
+import EditProductDialog from "../components/EditProductDialog";
 
 
 function home() {
